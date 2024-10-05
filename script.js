b1ac76ababe560eb2808565a2d0490af15bee346
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-10-06 01:44:00 +0200

    en cours ...portfolio

--- script.js
+++ script.js
@@ -69,4 +69,58 @@ $("#contact-form").submit(function(event) {
 
 
 
+// global. currently active menu item 
+var current_item = 0;
+
+// few settings
+var section_hide_time = 1300;
+var section_show_time = 1300;
+
+// jQuery stuff
+jQuery(document).ready(function($) {
+
+	// Switch section
+	$("a", '.mainmenu').click(function() 
+	{
+		if( ! $(this).hasClass('active') ) { 
+			current_item = this;
+			// close all visible divs with the class of .section
+			$('.section:visible').fadeOut( section_hide_time, function() { 
+				$('a', '.mainmenu').removeClass( 'active' );  
+				$(current_item).addClass( 'active' );
+				var new_section = $( $(current_item).attr('href') );
+				new_section.fadeIn( section_show_time );
+			} );
+		}
+		return false;
+	});		
+});
+
+
+// Globalement, l'élément du menu actuellement actif
+var current_item = 0;
+
+// Paramètres pour le temps d'affichage/masquage
+var section_hide_time = 1300;
+var section_show_time = 1300;
+
+jQuery(document).ready(function($) {
+
+    // Comportement lors du clic sur un lien du menu
+    $("a", '.mainmenu').click(function() {
+        if( ! $(this).hasClass('active') ) { 
+            current_item = this;
+            // Masque toutes les sections visibles avec la classe .section
+            $('.section:visible').fadeOut(section_hide_time, function() { 
+                $('a', '.mainmenu').removeClass('active');  
+                $(current_item).addClass('active');
+                // Affiche la nouvelle section
+                var new_section = $($(current_item).attr('href'));
+                new_section.fadeIn(section_show_time);
+            });
+        }
+        return false;
+    });
+});
+
 
